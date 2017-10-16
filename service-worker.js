3a90a111b58b111d6fa6ce20591e85933227bdbd
felipecota <felipe_cota@hotmail.com>
2017-10-16 17:04:50 -0200

    Offline fix

--- service-worker.js
+++ service-worker.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["/index.html","dcc7763fccf306e96797f25daeff3d8c"],["/inline.99b8c5940f693130cf6c.bundle.js","d265088940aa7be58bad3ac8ef7b5dff"],["/main.8658862dec51c5a972f5.bundle.js","a6daab97d48a8af7387d9e071fe60c85"],["/polyfills.550cf10c9aa54b8194c7.bundle.js","298be404e9e912caa292c56ffb02ae0a"],["/styles.d41d8cd98f00b204e980.bundle.css","d41d8cd98f00b204e9800998ecf8427e"],["/vendor.939b06fbb6c2d87128eb.bundle.js","5a670b274c3817deb6ce4086e6f34047"]];
+var precacheConfig = [["/index.html","cdc870f69c8448a8887f1edf04fe01d6"],["/inline.3fe3e31f608532dcf708.bundle.js","0ba3a8d9348ce912b0b197ecc8d357c9"],["/main.2d9caeb58ab67bec56b1.bundle.js","702ffe7c9b1005fc41dfb1b0da5a5e2d"],["/polyfills.550cf10c9aa54b8194c7.bundle.js","298be404e9e912caa292c56ffb02ae0a"],["/styles.d41d8cd98f00b204e980.bundle.css","d41d8cd98f00b204e9800998ecf8427e"],["/vendor.f4182d650b46079eab15.bundle.js","abf6a6db5da9bc1eb255e1ebc04b9d10"]];
 var cacheName = 'sw-precache-v3-sw-precache-' + (self.registration ? self.registration.scope : '');
 
 
